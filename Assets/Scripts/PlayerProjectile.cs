2e88ca72a1e6bac9ac3007049a7d489316b4cf45
Jacky Park <jackypark9852@gmail.com>
2026-01-28 22:14:46 -0500

    add projectile speedup

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
-    public float speed = 12f;
+    [Header("Speed Curve")]
+    public AnimationCurve speedCurve = AnimationCurve.EaseInOut(0f, 12f, 1f, 30f);
+    public float maxCurveTime = 1f;
     public float lifeTime = 5f;
+
+    float curveTimer;
+    float baseSpeed;
 
     void Start()
     {
+        baseSpeed = speedCurve.Evaluate(0f);
+        curveTimer = 0f;
         Destroy(gameObject, lifeTime);
     }
 
     void Update()
     {
-        transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+        curveTimer = Mathf.Min(curveTimer + Time.deltaTime, maxCurveTime);
+        float curveSpeed = speedCurve.Evaluate(curveTimer / maxCurveTime);
+        float currentSpeed = curveSpeed * Time.deltaTime;
+        
+        transform.Translate(Vector3.back * currentSpeed, Space.World);
     }
     
     void OnTriggerEnter(Collider other)
@@ -27,7 +38,7 @@ public class PlayerProjectile : MonoBehaviour
             }
             else
             {
-                UFO  ufo = other.GetComponent<UFO>();
+                UFO ufo = other.GetComponent<UFO>();
                 ufo.Kill();
             }
             Destroy(gameObject);
